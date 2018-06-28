1617fd77d22e0a68aa3553d6318e33e7819220bd
[ianizaguirre] <ian@izaguir.re>
2018-06-28 19:36:11 -0400

    Add: Showcard Link

--- js/ShowCard.jsx
+++ js/ShowCard.jsx
@@ -2,39 +2,34 @@
 
 import React from 'react';
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
-const Wrapper = styled.div`
-	width: 32%;
-	border: 2px solid #333;
-	border-radius: 4px;
-	margin-bottom: 25px;
-	padding-right: 10px;
-	overflow: hidden;
+const Wrapper = styled(Link)`
+  width: 32%;
+  border: 2px solid #333;
+  border-radius: 4px;
+  margin-bottom: 25px;
+  padding-right: 10px;
+  overflow: hidden;
+  color: black;
+  text-decoration: none;
 `;
 
 const Image = styled.img`
-	width: 46%;
-	float: left;
-	margin-right: 10px;
+  width: 46%;
+  float: left;
+  margin-right: 10px;
 `;
 
-const ShowCard = (props: {
-	poster: string,
-	title: string,
-	year: string,
-	description: string
-}) => (
-	<Wrapper>
-		<Image alt={`${props.title} Show Poster `} src={`/public/img/posters/${props.poster}`} />
-		<div>
-			<h3>{props.title}</h3>
-			<h4>({props.year})</h4>
-			<p>{props.description}</p>
-			<p> Something Different </p>
-		</div>
-	</Wrapper>
+const ShowCard = (props: Show) => (
+  <Wrapper to={`/details/${props.imdbID}`}>
+    <Image alt={`${props.title} Show Poster `} src={`/public/img/posters/${props.poster}`} />
+    <div>
+      <h3>{props.title}</h3>
+      <h4>({props.year})</h4>
+      <p>{props.description}</p>
+    </div>
+  </Wrapper>
 );
-
-
 
 export default ShowCard;

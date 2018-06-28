cb027d74188533b5b71c3793dabfab834798cc81
[ianizaguirre] <ian@izaguir.re>
2018-06-27 20:38:34 -0400

    Add: second half of conditional display logic for Header Component

--- js/Search.jsx
+++ js/Search.jsx
@@ -1,51 +1,40 @@
 // @flow
 
 import React, { Component } from 'react';
+import Header from './Header';
 import ShowCard from './ShowCard';
 
 class Search extends Component {
-	static defaultProps = {
-		// <-- DEFAULT PROPS
-		shows: [] // undefined gets converted to array,render won't trigger error
-	};
+  static defaultProps = {
+    // <-- DEFAULT PROPS
+    shows: [] // undefined gets converted to array,render won't trigger error
+  };
 
-	state = {
-		searchTerm: ''
-	};
+  state = {
+    searchTerm: ''
+  };
 
-	props: {
-		shows: Array<Show>
-	};
-	handleSearchTermChange = (
-		event: SyntheticKeyboardEvent & { target: HTMLInputElement }
-	) => {
-		this.setState({ searchTerm: event.target.value });
-	};
-	render() {
-		return (
-			<div className="search">
-				<header>
-					<h1>svideo</h1>
-					<input
-						onChange={this.handleSearchTermChange}
-						value={this.state.searchTerm}
-						type="text"
-						placeholder="Search"
-					/>
-				</header>
-				<div>
-					{this.props.shows
-						.filter(
-							show =>
-								`${show.title} ${show.description}`
-									.toUpperCase()
-									.indexOf(this.state.searchTerm.toUpperCase()) >= 0
-						)
-						.map(show => <ShowCard key={show.imdbID} {...show} />)}
-				</div>
-			</div>
-		);
-	}
+  props: {
+    shows: Array<Show>
+  };
+  handleSearchTermChange = (event: SyntheticKeyboardEvent & { target: HTMLInputElement }) => {
+    this.setState({ searchTerm: event.target.value });
+  };
+  render() {
+    return (
+      <div className="search">
+        <Header searchTerm={this.state.searchTerm} handleSearchTermChange={this.handleSearchTermChange} showSearch />
+        <div>
+          {this.props.shows
+            .filter(
+              show =>
+                `${show.title} ${show.description}`.toUpperCase().indexOf(this.state.searchTerm.toUpperCase()) >= 0
+            )
+            .map(show => <ShowCard key={show.imdbID} {...show} />)}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Search;

ad93bec4f0d5d2f698fc7c1c3bc5aba2b7a45330
[ianizaguirre] <ian@izaguir.re>
2018-06-28 23:45:36 -0400

    Add: Perf Tools and shouldComponentUpdate Method

--- js/client-app.jsx
+++ js/client-app.jsx
@@ -2,16 +2,20 @@
 
 import React from 'react';
 import { render } from 'react-dom';
+import Perf from 'react-addons-perf';
 import App from './App';
 
+window.Perf = Perf;
+Perf.start();
+
 const renderApp = () => {
-	render(<App />, document.getElementById('app'));
+  render(<App />, document.getElementById('app'));
 };
 
 renderApp();
 
 if (module.hot) {
-	module.hot.accept('./App', () => {
-		renderApp();
-	});
+  module.hot.accept('./App', () => {
+    renderApp();
+  });
 }

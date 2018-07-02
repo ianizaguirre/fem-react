7312cefcee9a0c310b8963d41d1da8d03add03a6
[ianizaguirre] <ian@izaguir.re>
2018-07-02 17:19:08 -0400

    Add: Redux in the Search component

--- js/Search.jsx
+++ js/Search.jsx
@@ -1,41 +1,25 @@
 // @flow
 
-import React, { Component } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 import Header from './Header';
 import ShowCard from './ShowCard';
 
-class Search extends Component {
-  static defaultProps = {
-    // <-- DEFAULT PROPS
-    shows: [] // undefined gets converted to array,render won't trigger error
-  };
+const Search = (props: {
+  searchTerm: string, // eslint-disable-line react/no-unused-prop-types
+  shows: Array<Show>
+}) => (
+  <div className="search">
+    <Header showSearch />
+    <div>
+      {props.shows
+        .filter(show => `${show.title} ${show.description}`.toUpperCase().indexOf(props.searchTerm.toUpperCase()) >= 0)
+        .map(show => <ShowCard key={show.imdbID} {...show} />)}
+    </div>
+  </div>
+);
+const mapStateToProps = state => ({
+  searchTerm: state.searchTerm
+});
 
-  state = {
-    searchTerm: ''
-  };
-
-  props: {
-    shows: Array<Show>
-  };
-  handleSearchTermChange = (event: SyntheticKeyboardEvent & { target: HTMLInputElement }) => {
-    this.setState({ searchTerm: event.target.value });
-  };
-  render() {
-    return (
-      <div className="search">
-        <Header searchTerm={this.state.searchTerm} handleSearchTermChange={this.handleSearchTermChange} showSearch />
-        <div>
-          {this.props.shows
-            .filter(
-              show =>
-                `${show.title} ${show.description}`.toUpperCase().indexOf(this.state.searchTerm.toUpperCase()) >= 0
-            )
-            .map(show => <ShowCard key={show.imdbID} {...show} />)}
-        </div>
-      </div>
-    );
-  }
-}
-
-export default Search;
+export default connect(mapStateToProps)(Search);

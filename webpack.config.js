49e986520e68f3a42da00a4c605ef021d5dc63a6
[ianizaguirre] <ian@izaguir.re>
2018-07-07 22:49:03 -0400

    Add: Server side rendering with HMR working

--- webpack.config.js
+++ webpack.config.js
@@ -2,49 +2,41 @@ const path = require('path');
 const webpack = require('webpack');
 
 module.exports = {
-	context: __dirname,
-	entry: [
-		'react-hot-loader/patch',
-		'webpack-dev-server/client?http://localhost:8080',
-		'webpack/hot/only-dev-server',
-		'./js/client-app.jsx',
-	],
-	devtool: 'cheap-eval-source-map',
-	output: {
-		path: path.join(__dirname, 'public'),
-		filename: 'bundle.js',
-		publicPath: '/public/'
-	},
-	devServer: {
-		hot: true,
-		publicPath: '/public/',
-		historyApiFallback: true
-	},
-	resolve: {
-		extensions: ['.js', '.jsx', '.json']
-	},
-	stats: {
-		colors: true,
-		reasons: true,
-		chunks: true
-	},
-	plugins: [
-		new webpack.HotModuleReplacementPlugin(),
-		new webpack.NamedModulesPlugin()
-	],
-	module: {
-		rules: [
-			{
-				enforce: 'pre',
-				test: /\.jsx?$/,
-				loader: 'eslint-loader',
-				exclude: /node_modules/
-			},
+  context: __dirname,
+  entry: ['webpack-hot-middleware/client?path=__webpack_hmr&timeout=2000', './js/client-app.jsx'],
+  devtool: 'cheap-eval-source-map',
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js',
+    publicPath: '/public/'
+  },
+  devServer: {
+    hot: true,
+    publicPath: '/public/',
+    historyApiFallback: true
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.json']
+  },
+  stats: {
+    colors: true,
+    reasons: true,
+    chunks: true
+  },
+  plugins: [new webpack.HotModuleReplacementPlugin(), new webpack.NamedModulesPlugin()],
+  module: {
+    rules: [
+      {
+        enforce: 'pre',
+        test: /\.jsx?$/,
+        loader: 'eslint-loader',
+        exclude: /node_modules/
+      },
 
-			{
-				test: /\.jsx?$/,
-				loader: 'babel-loader'
-			}
-		]
-	}
+      {
+        test: /\.jsx?$/,
+        loader: 'babel-loader'
+      }
+    ]
+  }
 };
